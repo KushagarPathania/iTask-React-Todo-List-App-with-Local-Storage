a03f8cda56ea4e296c7958f529ec1b46ff147444
KushagarPathania <kushagarpathania429@gmail.com>
2026-02-01 15:09:12 +0530

    ch

--- vite.config.js
+++ vite.config.js
@@ -6,5 +6,7 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [react(),
     tailwindcss(),
+    
   ],
+  base: '/iTask-React-Todo-List-App-with-Local-Storage/',
 })
